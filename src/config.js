bc8403190076c53c2c716db2ab7945f472649868
diegotech14 <diegoduarte8343@gmail.com>
2024-06-29 01:01:33 -0600

    changing env

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const PORT = process.env.PORT || 3000
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || ''
-export const DB_NAME = process.env.DB_NAME || ''
-export const DB_PORT = process.env.DB_PORT || 3306
+export const MYSQLHOST = process.env.MYSQLHOST || 'localhost'
+export const MYSQLUSER = process.env.MYSQLUSER || 'root'
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD || ''
+export const MYSQLDATABASE = process.env.MYSQLDATABASE || ''
+export const MYSQLPORT = process.env.MYSQLPORT || 3306
 export const EMAIL_ACCOUNT = process.env.EMAIL_ACCOUNT || ''
 export const EMAIL_PASS = process.env.EMAIL_PASS || ''
